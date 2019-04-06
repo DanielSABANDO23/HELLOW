43be301f0f9115196ee68cc8a218cc0482489f8d
Elkillax7 <aguerrero_spp@hotmail.com>
2019-04-06 13:00:06 -0500

    dfsdfv

--- HELLOW/HELLOW/Program.cs
+++ HELLOW/HELLOW/Program.cs
@@ -14,6 +14,8 @@ namespace HELLOW
         [STAThread]
         static void Main()
         {
+
+            //dsakjfhasdkiofdnhaj
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
